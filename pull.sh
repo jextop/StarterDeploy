e7c6def50912735eccddf8d7e0423093087234d9
dingxl <dingxl>
2020-01-14 22:47:08 +0800

    deploy admin

--- pull.sh
+++ pull.sh
@@ -5,4 +5,5 @@ docker pull registry.cn-shanghai.aliyuncs.com/hellodock/mysql:5
 docker pull registry.cn-shanghai.aliyuncs.com/hellodock/redis:4
 docker pull registry.cn-shanghai.aliyuncs.com/hellodock/activemq:latest
 
+docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_admin:latest
 docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_api:latest
