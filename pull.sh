009c2856528c077f3587e3913fce44fc72815bf0
dingxl <dingxl>
2020-02-04 15:44:34 +0800

    deploy with kibana

--- pull.sh
+++ pull.sh
@@ -5,7 +5,8 @@ docker pull adminer:latest
 docker pull redis:4
 docker pull webcenter/activemq:latest
 docker pull elasticsearch:latest
-docker pull registry.cn-shanghai.aliyuncs.com/hellodock/logstash:7.5.2
+docker pull registry.cn-shanghai.aliyuncs.com/hellodock/logstash:latest
+docker pull registry.cn-shanghai.aliyuncs.com/hellodock/kibana:latest
 
 docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_api:latest
 docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_admin:latest
