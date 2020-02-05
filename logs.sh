3c1ef9b7ef67e8f21313ee8b548a8170e51c8c3f
dingxl <dingxl>
2020-02-05 11:34:32 +0800

    Merge RUN to reduce layers of docker image

--- logs.sh
+++ logs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker-compose logs -ft | grep --color -i -e error -e warn -e version -e exception
+# docker service logs starter_api -ft | grep --color -i -e error -e warn -e version -e exception
