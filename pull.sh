990f750212f2397f0b14d618e1ad5852ca037055
dingxl <dingxl>
2020-02-07 09:33:22 +0800

    Deploy grafana and prometheus

--- pull.sh
+++ pull.sh
@@ -5,8 +5,10 @@ docker pull adminer:latest
 docker pull redis:4
 docker pull webcenter/activemq:latest
 docker pull elasticsearch:latest
+docker pull grafana/grafana:latest
 docker pull registry.cn-shanghai.aliyuncs.com/hellodock/logstash:latest
 docker pull registry.cn-shanghai.aliyuncs.com/hellodock/kibana:latest
+docker pull registry.cn-shanghai.aliyuncs.com/hellodock/prometheus:latest
 
 docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_api:latest
 docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_admin:latest
