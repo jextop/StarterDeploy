f749293effcbf5fad220e1d7b1f211451616b5e9
dingxl <dingxl>
2020-02-12 12:23:40 +0800

    add alert manager and update log stash

--- pull.sh
+++ pull.sh
@@ -5,9 +5,10 @@ docker pull adminer:latest
 docker pull redis:4
 docker pull webcenter/activemq:latest
 docker pull elasticsearch:latest
-docker pull grafana/grafana:latest
 docker pull registry.cn-shanghai.aliyuncs.com/hellodock/logstash:latest
 docker pull registry.cn-shanghai.aliyuncs.com/hellodock/kibana:latest
+docker pull grafana/grafana:latest
+docker pull registry.cn-shanghai.aliyuncs.com/hellodock/alertmanager:latest
 docker pull registry.cn-shanghai.aliyuncs.com/hellodock/prometheus:latest
 
 docker pull registry.cn-shanghai.aliyuncs.com/jext/starter_api:latest
